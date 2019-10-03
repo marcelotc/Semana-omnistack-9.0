813ff8a0dc1d973f6c029060fe38b94aed72720f
Marcelo T. Cortes <marceloteixeiracortes@gmail.com>
2019-10-02 21:13:04 -0300

    Adicionado cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const app = express();
@@ -9,7 +11,8 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(3333);
